44407ccfac5461c3cc609739ad8d36aa6d2b76f5
cesarruiz <cruiz85@gmail.com>
2019-12-15 11:34:22 +0100

    creation of the spring boot rest api, first commit

---
+++ src/test/java/com/fonyou/demo/FonyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonyou.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
